f228d767f60cf8dd4a4c2974ee62d3c87c9c8fb2
Matt McGee <mlmcgeenc@gmail.com>
2022-06-14 12:15:51 -0400

    add player getStats and getInventory methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,8 +3,6 @@ const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
-console.log(new Potion())
-
 test('creates a new player', () => {
 	const player = new Player('Dave');
 
@@ -13,5 +11,24 @@ test('creates a new player', () => {
 	expect(player.strength).toEqual(expect.any(Number));
 	expect(player.agility).toEqual(expect.any(Number));
 
-  expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+	expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
+
+test('gets player stats as an object', () => {
+	const player = new Player('Dave');
+
+	expect(player.getStats()).toHaveProperty('potions');
+	expect(player.getStats()).toHaveProperty('health');
+	expect(player.getStats()).toHaveProperty('strength');
+	expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('get player inventory or if invetory is empty return false', () => {
+  const player = new Player('Dave')
+
+  expect(player.getInventory()).toEqual(expect.any(Array))
+
+  player.inventory = []
+
+  expect(player.getInventory()).toEqual(false)
+})
